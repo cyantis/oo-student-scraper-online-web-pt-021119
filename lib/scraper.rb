2aa58dea81582b5429de1a15dbe950c49078d199
Cody Yantis <cyantis@gmail.com>
2019-03-30 22:41:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,8 +21,22 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student_url = Nokogiri::HTML(open(profile_url))
     social_media = student_url.css(".social-icon-container")
+    url_array = social_media.children.css("a").collect { |a| a.attribute("href").text}
     profile_hash = {}
-    binding.pry
+    url_array.each do |url|
+      if url.include?("twitter")
+        profile_hash[:twitter] = url
+      elsif url.include?("linkedin")
+        profile_hash[:linkedin] = url
+      elsif url.include?("github")
+        profile_hash[:github] = url
+      else
+        profile_hash[:blog] = url
+      end
+    end
+    profile_hash[:profile_quote] = student_url.css(".profile-quote").text
+    profile_hash[:bio] = student_url.css(".bio-block p").text
+    profile_hash
   end
 
 end
