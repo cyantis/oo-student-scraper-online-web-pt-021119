d17f891130abe4640480693aeba9c129f6b38db3
Cody Yantis <cyantis@gmail.com>
2019-03-30 23:03:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,8 +14,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.collect do |hash, data|
-      binding.pry
+    students_array.collect do |hash|
+      Student.new(hash)
     end
   end
 
