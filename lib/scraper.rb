586517802ac890bc2c483b0059b2a7984014b136
Cody Yantis <cyantis@gmail.com>
2019-03-30 21:25:27 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,23 +3,26 @@ require 'pry'
 
 class Scraper
 
-def self.scrape_index_page(index_url)
-  students = Nokogiri::HTML(open(index_url))
-  student_array = []
-  students.css(".roster-cards-container").each do |card|
-    card.css(".student-card a").each do |student|
-      student_array << {
-      :name => student.css(".student-name").text,
-      :location => student.css(".student-location").text,
-      :profile_url => student.attribute("href").text
-    }
+  def self.scrape_index_page(index_url)
+    students = Nokogiri::HTML(open(index_url))
+    student_array = []
+    students.css(".roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_array << {
+        :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.attribute("href").text
+      }
+      end
     end
+    student_array
   end
-  student_array
-end
 
   def self.scrape_profile_page(profile_url)
-
+    student_url = Nokogiri::HTML(open(profile_url))
+    social_media = student_url.css(".social-icon-container")
+    profile_hash = {}
+    binding.pry
   end
 
 end
